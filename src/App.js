517740b5b57db04154e8a4ba95fcf580b83661b9
Evinym <Evinym@outlook.fr>
2018-01-18 12:16:41 -0400

    projet simplon LMDA

--- src/App.js
+++ src/App.js
@@ -9,14 +9,16 @@ state={
 }
   render() {
     return (
-      <div className="All">
-        <div className="All2" >
+      <div className="page-error-container">
+        <div className="page-error-container2" >
           <div className="dinheader">
           <img className="dinheaders" src={LMDA} alt="logo" height="178px" width="414px"/>
           </div>
+          <div className="dinheador dinheaderset">
           <p className="oups" ><strong>Oupss!</strong></p>
-          <p>La page que vous recherchez n’existe pas ou plus :</p>
-          <p>Utiliser le menu ci-dessus ou retourner à la page d’accueil pour tenter de trouver l’article que vous poipi.</p>
+          <p>La page que vous recherchez n’existe pas ou plus :</p>          
+          <p>Utiliser le menu ci-dessus ou retourner à la page d’accueil pour tenter de trouver l’article que vous chercher.</p>
+          </div>
         </div>
       </div>
     );
